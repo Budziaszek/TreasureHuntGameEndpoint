3ef2052601f852365b54a0aea858385dc08ff70e
Budziaszek <budziaszek@gmail.com>
2020-11-28 16:33:34 +0100

    Check position - passing current location and email.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Home page"
+
+
+@app.route('/treasure_hunt.json')
+def check_position():
+    return "Position: {}. Email: {}".format(request.args.getlist('current_location[]', None),
+                                            request.args.get("email", None))
 
 
 if __name__ == '__main__':
