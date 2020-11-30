f422ba526238b00b28567929990b4a14bcb046e8
Budziaszek <budziaszek@gmail.com>
2020-11-30 18:12:39 +0100

    Creating database for user data. Sending email only once.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 import re
 from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from math import cos, asin, pi, sqrt
 
@@ -13,6 +14,19 @@ app.config['MAIL_PASSWORD'] = '****'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/treasure_hunt"
+db = SQLAlchemy(app)
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    found_treasure = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<Player %r>' % self.email
 
 
 @app.route('/')
@@ -28,7 +42,7 @@ def check_position():
     validate_email(email)
     distance = calculate_distance(position, treasure_location)
     if distance <= 5:
-        send_email(email, 0)
+        send_email(email)
     return jsonify(status="ok", distance=distance)
 
 
@@ -37,12 +51,38 @@ def page_not_found(error):
     return jsonify(status="error", distance=-1, error=error.description), 404
 
 
-def send_email(email, n):
+def get_found_treasure(email):
+    players = Player.query.filter(Player.email == email).all()
+    return len(players) > 0 and players[0].found_treasure is True
+
+
+def add_player(email):
+    if len(db.session.query(Player).filter(Player.email == email).all()) == 0:
+        player = Player(email=email, found_treasure=False)
+        db.session.add(player)
+        db.session.commit()
+
+
+def set_found_treasure(email):
+    add_player(email)
+    db.session.query(Player).filter(Player.email == email).update({Player.found_treasure: True},
+                                                                  synchronize_session=False)
+    db.session.commit()
+
+
+def get_players_count():
+    return len(db.session.query(Player).filter(Player.found_treasure == 'true').all())
+
+
+def send_email(email):
+    if get_found_treasure(email):
+        return
     msg = Message('Congratulations!', sender='@gmail.com', recipients=[email])
     msg.body = "Hey, you’ve found a treasure, congratulations! You are {} treasure hunter who has found the treasure." \
                "\nTreasure position is: {}"\
-        .format(n, treasure_location)
+        .format(get_players_count() + 1, treasure_location)
     mail.send(msg)
+    set_found_treasure(email)
 
 
 def validate_position(position):
@@ -67,4 +107,5 @@ def calculate_distance(point_1, point_2):
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
